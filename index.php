b5d9fa1a15850e892cb0d6020defd90151d450ba
mari1811 <40413045+mari1811@users.noreply.github.com>
2018-11-16 23:22:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
-
 <?php 
 echo "<!DOCTYPE html>\n"; 
-echo "<html>\n";
+echo "<html>\n"; 
 echo " <head>\n"; 
 echo " <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"; 
-echo " <title>JSP Page</title>\n"; 
+echo " <title>Login</title>\n"; 
 echo " <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"; 
 echo " <link href=\"css/newcss.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"; 
-echo " </head>\n";
-echo " <body background=\"imagenes/fondo2.jpg\"> \n";
+echo " </head>\n"; 
+echo " <body background=\"imagenes/fondo2.jpg\" > \n"; 
+echo " <center><img style=\"float:top; margin:1px;\" alt=\"\" src=\"imagenes/mapa5.png\" width=\"70\" height=\"70\" />\n"; 
+echo " </center> <font face=\"SimSun-ExtB\" size=\"7\" color=\"black\"> <center id=\"titulo1\">PackTex</center></font>\n"; 
 echo " <div class=\"container well\"id=\"login\"> \n"; 
 echo " <form>\n"; 
-echo " <div class=\"form-group\">\n"; 
-echo " <input type=\"email\" class=\"form-control\" id=\"exampleInputEmail1\" placeholder=\"Email\">\n"; 
+echo " <div class=\"form-group\">\n";
+echo " <input type=\"email\" class=\"form-control input-lg\" id=\"exampleInputEmail1\" placeholder=\"E-mail\">\n"; 
 echo " </div>\n"; 
 echo " <div class=\"form-group\">\n"; 
-echo " <input type=\"password\" class=\"form-control\" id=\"exampleInputPassword1\" placeholder=\"Password\">\n";
+echo " <input type=\"password\" class=\"form-control input-lg\" id=\"exampleInputPassword1\" placeholder=\"Clave\">\n"; 
 echo " </div>\n"; 
-echo " <center> <button type=\"submit\" class=\"btn btn-primary\"id=\"iniciar\">Iniciar sesión</button></center>\n"; 
+echo " <center> <button type=\"submit\" class=\"btn btn-primary btn-lg\"id=\"iniciar\">Iniciar sesión</button></center>\n"; 
 echo " <text>¿No tienes una cuenta? </text>\n"; 
-echo "<a href=\"https://p124.herokuapp.com/registar.php\" >Registrate</a>\n"; 
+echo "<a href=\"registrar.jsp\" >Registrate</a>\n"; 
 echo "</form> \n"; 
 echo " </div>\n"; 
-echo " </body>\n"; 
+echo " </body>\n";
 echo "</html>\n"; ?>
