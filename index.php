9eeb91d92e7ad3d4dd7ec91ce37414b36a5e2234
mari1811 <40413045+mari1811@users.noreply.github.com>
2019-01-19 17:54:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ echo "	<div class=\"six\">\n";
 echo "	<br>\n";
 echo "	<center><img src=\"imagen1.png\" class=\"img-rounded\" alt=\"Cinque Terre\" width=\"500\" height=\"236\"></center>\n";
 echo "	<br>\n"; 
-echo "	<a href=\"evento.html\"><center><button type=\"submit\" style='width:190px; height:40px' class=\"btn btn-primary btn-lg\">Ver más</button></center></a>\n"; echo "	</div><!--closing column 6-->\n";
+echo "	<a href=\"https://p124.herokuapp.com/evento.html\"><center><button type=\"submit\" style='width:190px; height:40px' class=\"btn btn-primary btn-lg\">Ver más</button></center></a>\n"; echo "	</div><!--closing column 6-->\n";
 echo "\n"; 
 echo "	<div class=\"six\">\n"; 
 echo "	<br>\n"; 
